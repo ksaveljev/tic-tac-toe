29f3839d58cbed177d0819350fe27caefa1356ca
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2014-12-10 12:03:56 +0200

    Game tree generation

--- src/TicTacToe.hs
+++ src/TicTacToe.hs
@@ -1,5 +1,6 @@
 module TicTacToe where
 
+import Data.Tree (Tree(..), unfoldTree)
 import Data.List (foldl', intercalate)
 import Data.Array.IArray (Array, listArray, indices, elems, (!), (//))
 
@@ -78,3 +79,57 @@ pprint = putStrLn . pretty
 -- | Return a one-line string representation of the state
 prettyOneLine :: State -> String
 prettyOneLine = concatMap show . cells
+
+-- | Examine the board and return whose turn it is, X or O
+getTurn :: State -> Cell
+getTurn st
+  | k == 0    = XX
+  | k == 1    = OO
+  | otherwise = error "encountered invalid board state"
+  where
+    k = count XX st - count OO st
+    count c = length . filter (== c) . cells
+
+-- | A collection of cell extration functions
+getRow :: State -> Int -> [Cell]
+getRow s i = [ lookupCell s (i,j) | j <- [0,1,2] ]
+
+getCol :: State -> Int -> [Cell]
+getCol s i = [ lookupCell s (j,i) | j <- [0,1,2] ]
+
+getDiag :: State -> [Cell]
+getDiag s = [ lookupCell s (i,j) | (i,j) <- [(0,0), (1,1), (2,2)] ]
+
+getAntiDiag :: State -> [Cell]
+getAntiDiag s = [ lookupCell s (i,j) | (i,j) <- [(2,0), (1,1), (0,2)] ]
+
+-- | Check if the given player has won the game
+win :: Player -> State -> Bool
+win player state = checkRows || checkCols || checkDiag || checkAntiDiag
+  where
+    c = piece player
+    checkWin v = all (== v)
+    checkRows = or [checkWin c $ getRow state i | i <- [0,1,2]]
+    checkCols = or [checkWin c $ getCol state i | i <- [0,1,2]]
+    checkDiag = checkWin c $ getDiag state
+    checkAntiDiag = checkWin c $ getAntiDiag state
+
+-- | Generate a list of new states reachable from the given state in one move
+moves :: State -> [State]
+moves st
+  | win PlayerX st || win PlayerO st = []
+  | otherwise = map (\p -> update st (p, getTurn st)) (freePos st)
+
+-- | Generate a game tree from an initial game state
+generate :: State -> Tree State
+generate = unfoldTree (\s -> (s, moves s))
+
+-- | A general purpose tree fold
+foldTree :: (a -> b -> a) -> (a -> a -> a) -> a -> Tree b -> a
+foldTree f _ a (Node b []) = f a b
+foldTree f g a (Node b (t:rest)) = g (foldTree f g a t) (foldTree f g a (Node b rest))
+
+-- | A general purpose tree pruner
+prune :: Int -> Tree a -> Tree a
+prune 0 t = Node (rootLabel t) []
+prune n t = Node (rootLabel t) (map (prune (n-1)) (subForest t))
