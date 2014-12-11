c7190c9e17e3a2672a16e23665124a7c3732458c
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2014-12-11 16:29:52 +0200

    Showcase of using the alpha-beta search to find next move

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,16 @@
+import Control.Monad (forM_)
+
 import TicTacToe
 
+printMoves :: State -> IO ()
+printMoves st = forM_ (zip [0..] (moves st)) $ \(i, s) -> do
+  print i
+  pprint s
+  putStrLn ""
+
 main :: IO ()
-main = undefined
+main = do
+    let st = posSeq [(1,1), (1,0), (0,2), (2,0)]
+    pprint st
+    print $ map abmax $ moves st
+    printMoves st
